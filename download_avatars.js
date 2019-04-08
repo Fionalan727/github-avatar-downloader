9e95239d43924cda344aaf6b7ddff3baa0f9a968
Yufei Lan <lanyufei518@gmail.com>
2019-04-08 23:11:51 +0000

    added command line argument

--- download_avatars.js
+++ download_avatars.js
@@ -1,20 +1,39 @@
 var request = require('request');
+var secret = require('./secret');
+var fs = require('fs');
 console.log('Welcome to the Github Avatar Downloader!');
-
+var owner = process.argv[2];
+var repo = process.argv[3];
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      Authorization: 'token' + secret.GITHUB_TOKEN
+      Authorization: 'token ' + secret.GITHUB_TOKEN
     }
+  
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    var data = JSON.parse(body);
+    cb(err, data);
   });
 }
-getRepoContributors("jquery", "jquery", function(err, result) {
+
+function downloadImageByURL(url, filePath) {
+  request.get(url).pipe(fs.createWriteStream(filePath));
+         
+}
+
+getRepoContributors(owner, repo, function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  //console.log("Result:", result);
+  result.forEach(function(result) {
+    var avatarURL = result.avatar_url;
+    var file = './avatars/' + result.login + '.jpg';
+    downloadImageByURL(avatarURL, file)
   });
+   
+});
+
+  
