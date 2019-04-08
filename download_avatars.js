402a3f9392956bf2b91ff9851a15854ee6a18afb
Yufei Lan <lanyufei518@gmail.com>
2019-04-08 21:50:21 +0000

    secret.js added and header added

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,18 @@ var request = require('request');
 console.log('Welcome to the Github Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    // ...
-  }
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      Authorization: 'token' + secret.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
